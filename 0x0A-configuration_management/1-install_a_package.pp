4200c2e6a9ab9bb535974755a59d6ec903e47f1f
ElyasYid <elyassayle27@gmail.com>
2024-03-22 02:57:59 -0700

    akas

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Install Flask version 2.1.0
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+#Install Flask v 2.1.0
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => ['/usr/bin'],
+  unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
